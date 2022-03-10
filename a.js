df67d7937d428f54116fa597be7549256c499e2e
Bigborg <100381400+EnzoBDF@users.noreply.github.com>
2022-03-10 12:55:58 -0300

    Add files via upload

---
+++ a.js
@@ -0,0 +1,57 @@
+nome1 = "pedro"
+nome2 = "Lohane"
+Sobrenome1 = "Anderson"
+Sobrenome2 = "Vekanandre "
+console.log(nome1, Sobrenome1)
+console.log(nome2, Sobrenome2)
+
+nome3= "bernardo machado"
+nome4="Jaíza Freire"
+nome5="Ana carolina"
+nome6="maria Vênus"
+nome7="Cairé"
+nome8="Paula Tejando"
+
+cancelou = true
+lacrou = false
+cozinhou = false
+Militou = true
+berrou = true
+vazou = false
+
+console.log(nome3 , lacrou)
+console.log(nome4 , cancelou)
+console.log(nome5 , vazou)
+console.log(nome6 , Militou)
+console.log(nome7 , vazou)
+console.log(nome8 , berrou)
+
+
+numero1 = "1"
+numero2 = "2"
+numero3 = "3"
+numero4 = "4"
+numero5 = "5"
+numero6 = "6"
+numero7 = "7"
+numero8 = "8"
+console.log(numero1, numero2, numero3, numero4, numero5, numero6, numero7, numero8)
+
+
+const subtrair = function (x, y){
+    let resultado = x - y
+    return "Subtração: (" + x + ") - (" + y + ") = " + resultado
+   
+   }
+   console.log(subtrair(20 , 40))
+
+   const dividir = function (x, y){
+    let resultado = x / y
+    return "Divisão: (" + x + ") / (" + y + ") = " + resultado
+   
+   }
+   console.log(dividir(20 , 40))
+
+
+ 
+
