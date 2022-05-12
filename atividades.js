5cc41b0302dd22a79ef6b5cf11be8ba741718796
Bigborg <100381400+EnzoBDF@users.noreply.github.com>
2022-05-12 12:51:15 -0300

    Add files via upload

---
+++ atividades.js
@@ -0,0 +1,25 @@
+
+//1
+let nomes = ['Paulo', 'Joana', 'pedrao', 'Jamile', 'Artur']
+console.log(nomes)
+
+//2
+let salarios = ["Salarios dos funcionarios",'R$ 90,00', 'R$ 980,00', 'R$ 650,00', 'R$ 120,00', 'R$ 660,00', 'R$ 230,00', 'R$ 760,00', 'R$ 900,00', 'R$ 80,00',]
+console.log(salarios)
+
+
+//3
+let booleanas = [1  > 2, 3 > 8, 5 < 6]
+console.log(booleanas)
+
+//4
+
+let matriz = [[2,4,8]]
+let preço = ["10"]
+for(let i in matriz){
+    matriz [i][0] *= preço
+    matriz [i][1] *= preço
+    matriz [i][2] *= preço
+}
+console.log(matriz)
+
